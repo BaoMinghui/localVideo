781cf8cd67d5d5ed836f5de923181ad76399617c
Bao Minghui <var_bmh@163.com>
2018-04-29 22:50:36 +0800

    添加评分功能

--- models/videoData.js
+++ models/videoData.js
@@ -8,7 +8,9 @@ var videoSchema = new Schema({
   createTime: {
     type: Date,
     default: moment().format('YYYY-MM-DD HH:mm:ss')
-  }
+  },
+  score: Number,
+  delete: Boolean,
 })
 
 
@@ -18,33 +20,64 @@ videoSchema.statics = {
       skip: (page - 1) * limit,
       limit: limit
     })
-    let total = Math.ceil(await this.count()/limit)
-    return {total,data}
+    let total = Math.ceil(await this.count() / limit)
+    return {
+      total,
+      data
+    }
+  },
+
+  get_data_random: async function(limit) {
+    let videoList = []
+    for (let i = 0; i < limit; i++) {
+      let id = Math.ceil(Number(await this.count()) * Math.random())
+      let data = await this.findOne({
+        id: id
+      },'-_id name id')
+      videoList.push(data)
+    }
+    return videoList
   },
 
   get_url: async function(videoId) {
     let video = await this.findOne({
-      id:videoId
+      id: videoId
     })
     return video.url
   },
 
-  set_score: async function(videoId,score){
-    let data = await this.findOneAndUpdate({id:videoId},{$set:{score:score}},{new:true})
-    if (data.score === score) {
-      return true
+  set_score: async function(videoId, score) {
+    let video = await this.findOneAndUpdate({
+      id: videoId
+    }, {
+      $set: {
+        score: score
+      }
+    }, {
+      new: true
+    })
+    video.save()
+    return video
+  },
+
+  get_score: async function(videoId) {
+    let video = await this.findOne({
+      id: videoId
+    })
+    if (video.score) {
+      return video.score
+    } else {
+      return false
     }
   },
 
-  get_score:async function(videoId){
-    let data = await this.findOne({
-      id:videoId
+  set_delete: async function(videoId) {
+    let video = await this.findOne({
+      id: videoId
     })
-    if(data.score){
-      return data.score
-    }else{
-      return false
-    }
+    video.delete = !vidoe.delete
+    video.save()
+    return video.delete
   }
 }
 
--- router/video.js
+++ router/video.js
@@ -8,10 +8,10 @@ let video = new Router()
 video.get('/', async (ctx, next) => {
   let page = ctx.query.page
   let limit = Number(ctx.query.limit)
-  if(!page||!limit){
+  if (!page || !limit) {
     ctx.status = 500
     ctx.body = "Lack of necessary conditions"
-  }else{
+  } else {
     let videoData = await ctx.model('video').get_data(page, limit)
     videoData.status = 1
     ctx.body = videoData
@@ -19,42 +19,81 @@ video.get('/', async (ctx, next) => {
   await next()
 })
 
+//获取随机列表
+video.get('/random',async (ctx,next)=>{
+  let limit = ctx.query.limit
+  let videolist = await ctx.model('video').get_data_random(limit)
+  ctx.body = videolist
+  await next()
+})
+
 //视频评分
-video.get('/score',async(ctx,next)=>{
-    let id = parseInt(ctx.params.id)
-    let score = await ctx.model('video').get_score(id)
-    let data = {}
-    if(score){
-      data = {
-        status:1,
-        score
-      }
-    }else{
-      data = {
-        status:0
-      }
+video.get('/score', async (ctx, next) => {
+  let id = Number(ctx.query.id)
+  let score = await ctx.model('video').get_score(id)
+
+  if (score) {
+    ctx.body = {
+      status: 1,
+      score
     }
-    ctx.body = data
-    await next()
+  } else {
+    ctx.body = {
+      status: 0
+    }
+  }
+
+  await next()
 })
 
 
 //设置视频评分
-video.post('/setScore',koaBody(),async(ctx,next)=>{
-  ctx.body = ctx.request.body.score
+video.post('/setScore', koaBody(), async (ctx, next) => {
+
+  let data = ctx.request.body
+  let id = Number(data.id)
+  let score = Number(data.score)
+  let scoreNew = await ctx.model('video').set_score(id, score)
+  ctx.body = {
+    score: scoreNew.score
+  }
   await next()
 })
 
+//投入回收站
+video.post('/setDelete', koaBody(), async (ctx, next) => {
+  let body = ctx.request.body
+  let id = Number(body.id)
+  let data = await ctx.model('video').set_delete(id)
+  if (data) {
+    ctx.body = {
+      status: 1
+    }
+  } else {
+    ctx.body = {
+      status: 0
+    }
+  }
+  await next()
+})
+
+//恢复视频
+video.get('/setDelete', async (ctx,next)=>{
+  let id = ctx.query.id
+
+})
+
 //视频播放
-video.get('/:id', async (ctx, next) => {
+video.get('/play/:id', async (ctx, next) => {
   let id = parseInt(ctx.params.id)
   let realpath = await ctx.model('video').get_url(id)
 
   if (realpath) {
-    await readFile(ctx,realpath)
+    await readFile(ctx, realpath)
   }
   await next()
 })
+
 
 let readFile = async (ctx, realpath) => {
   let match = ctx.request.header['range']
